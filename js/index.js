711fa3371d39d20b460b8d12d0755cf5b5c28143
Eber <www.eberacunaquispe@gmail.com>
2026-09-10 17:55:00 -0500

    Script principal del proyecto

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const inicio = document.querySelector('.inicio');
+const nosotros = document.querySelector('.nosotros');
+const servicios = document.querySelector('.servicios');
+const horarios = document.querySelector('.horarios');
+const test = document.querySelector('.test');
+const contenido = document.getElementById('contenido');
+
+function clean() {
+    contenido.textContent = '';
+}
+
+inicio.addEventListener('click', () => {});
