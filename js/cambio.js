1d130855ed99717ced9e138782edc88d7c3f8fbd
Eber <www.eberacunaquispe@gmail.com>
2026-09-10 18:43:08 -0500

    funcion asincrona que permite encontrar los modulos en la carpeta pages y cambiarlos dinamicamente en base a lo selecionado en la barra de navegacion

---
+++ js/cambio.js
@@ -0,0 +1,8 @@
+const contenido = document.getElementById('contenido');
+
+export async function cargarmodulo(pagina) {
+    const promesa = await fetch(`../pages/${pagina}.html`);
+    const modulo = await promesa.text();
+
+    contenido.innerHTML = modulo;
+}
