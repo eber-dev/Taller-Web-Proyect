944dd9e779909c5ae4fe6dada9c2476a4021f5fa
Eber <www.eberacunaquispe@gmail.com>
2026-09-19 00:56:16 -0500

    Se borro la funcion clean innerhtml ya reemplaza el contenido del div al intercambiar

--- js/index.js
+++ js/index.js
@@ -8,35 +8,25 @@ const nosotros = document.querySelector('.nosotros');
 const servicios = document.querySelector('.servicios');
 const horarios = document.querySelector('.horarios');
 const test = document.querySelector('.test');
-const contenido = document.getElementById('contenido');
-
-function clean() {
-    contenido.textContent = '';
-}
 
 inicio.addEventListener('click', () => {
-    clean();
     cargarmodulo('inicio');
 });
 
 nosotros.addEventListener('click', () => {
-    clean();
     cargarmodulo('nosotros');
 });
 
 servicios.addEventListener('click', () => {
-    clean();
     cargarmodulo('servicios');
 });
 
 horarios.addEventListener('click', async () => {
-    clean();
     await cargarmodulo('horarios');
     iniciarHorario();
 });
 
 test.addEventListener('click', () => {
-    clean();
     cargarmodulo('test');
 });
 
