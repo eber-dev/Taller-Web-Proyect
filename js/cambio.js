5a889e771caf4a318f217f08f8816298878e21f6
Eber <www.eberacunaquispe@gmail.com>
2026-09-19 23:56:20 -0500

    Corregir rutas de carga de páginas

--- js/cambio.js
+++ js/cambio.js
@@ -1,7 +1,7 @@
 const contenido = document.getElementById('contenido');
 
 export async function cargarmodulo(pagina) {
-    const promesa = await fetch(`../pages/${pagina}.html`);
+    const promesa = await fetch(`./pages/${pagina}.html`);
     const modulo = await promesa.text();
 
     contenido.innerHTML = modulo;
